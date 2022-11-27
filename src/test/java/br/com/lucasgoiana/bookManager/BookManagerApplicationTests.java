46c640225063b6a22de437287fc7cb904f51feea
LucasGoianaMalicia <lucasgoianam@hotmail.com>
2022-11-27 12:37:10 -0300

    first commit

---
+++ src/test/java/br/com/lucasgoiana/bookManager/BookManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucasgoiana.bookManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
